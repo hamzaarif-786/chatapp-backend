f2072e7e70a45f2b17325de5796cf52c78892619
hamzaarif-786 <arifhamza186@gmail.com>
2025-12-18 21:37:51 +0500

    Update CORS origin for production deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 // 🔴 MIDDLEWARE MUST COME FIRST
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"chatapp-frontend-alpha-one.vercel.app",
   credentials:true
 }))
 app.use(express.json());
